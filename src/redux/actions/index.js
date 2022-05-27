568f4a469fbb089a26ac4f843204eb275cdad293
WBomfim <willianbomfim@hotmail.com>
2022-05-27 15:39:34 -0300

    Feat: Criado o botão next para exibir a próxima questão.

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,12 @@
 // Types
 export const CHANGE_USER = 'CHANGE_USER';
+export const NEXT_TRUE = 'NEXT_TRUE';
 
 export const changeUser = (state) => ({
   type: CHANGE_USER,
   payload: state,
+});
+
+export const nextTrue = () => ({
+  type: NEXT_TRUE,
 });
---
+++ src/redux/reducers/game.js
@@ -0,0 +1,18 @@
+import { NEXT_TRUE } from '../actions';
+
+const INITIAL_STATE = {
+  isNext: false,
+};
+
+const game = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case NEXT_TRUE:
+    return {
+      ...state,
+      isNext: true,
+    };
+  default: return state;
+  }
+};
+
+export default game;
